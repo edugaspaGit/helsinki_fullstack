0dfcf3c8b5ce28d473418dd143b9839a061fec20
Edu Gaspa <eduardogasparotto@hotmail.com>
2024-09-13 18:51:39 -0300

    Coursedata Part2 Exercise

---
+++ part2/coursedata/src/components/Course.jsx
@@ -0,0 +1,40 @@
+const Header = ({ name }) => {
+  return <h3>{name}</h3>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => {
+        return <Part key={part.id} part={part} />;
+      })}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, value) => {
+    return sum + value.exercises;
+  }, 0);
+  return <h4>Total {total}</h4>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
